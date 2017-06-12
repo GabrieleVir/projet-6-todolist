891e6542eb5439e86550eb15e2d1225b56ae5871
GabrieleVir <Gabriele_Virga@hotmail.com>
2017-06-12 16:47:51 +0200

    Json decode done

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>To do list</title>
+</head>
+<body>
+	<form action="formulaire-traitement.php" method="POST">
+		<label for="toDoText">La tâche a effectuer</label>
+		<input type="text" id = "toDoText" name="toDoText">
+		<input type="submit" name="submitBtn" value="Ajouter">
+	</form>
+</body>
+</html>
