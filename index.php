4bdd7c6a93246a111172fa576d7a3115ddb36d9e
GabrieleVir <Gabriele_Virga@hotmail.com>
2017-06-14 16:02:32 +0200

    Version qui s'affiche au moins

--- index.php
+++ index.php
@@ -4,11 +4,10 @@ include "fonctions.php";
 if(isset($_POST['submit'])) { //isset = si le bouton submit et appuyé alors le code s'execute
 	include "ecrire-JSON.php";
 	//On va rechercher $tachesEncode Il faut d'abord récuperer ce qu'il y a dans todo.json avant json_decode 
-	
 	$stock_json_decode = json_decode($taches_encode); //transformer json en language php !!!! en array avec des objets à l'intérieur
  	//pr($stock_json_decode);
  	
-
+}
 ?>
 
 <!DOCTYPE html>
@@ -28,8 +27,8 @@ if(isset($_POST['submit'])) { //isset = si le bouton submit et appuyé alors le 
 		
 		
 		?>
-			<label for="TachesAFaire">
-			<input type="checkbox" name="TachesAFaire" id="TachesAFaire"></label><br>
+			<label for="TachesAFaire"></label>
+			<input type="checkbox" name="TachesAFaire" id="TachesAFaire"><br>
 
 		
 		<?php
@@ -45,8 +44,3 @@ if(isset($_POST['submit'])) { //isset = si le bouton submit et appuyé alors le 
 </body>
 </html>
 
-
-
-<?php //FERME ISSET Submit
-} 
-?>
